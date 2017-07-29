fdd9175c9506c677e089cbd9eb42abc47073037b
TheNotary <no@email.plz>
2017-07-29 11:49:30 -0500

    lists VMs

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ import sys
 
 import pyVim
 from pyVim import connect
+from pyVmomi import vim
 
 def main():
     esxi_vsphere_server, esxi_user, esxi_pass = lookup_env_variables()
 
     my_cluster = connect.ConnectNoSSL(esxi_vsphere_server, 443, esxi_user, esxi_pass)
+
+    list_vms(my_cluster)
+
     connect.Disconnect(my_cluster)
 
 
@@ -25,6 +29,63 @@ def lookup_env_variables():
         #import pdb; pdb.set_trace()
         #traceback.print_exception(*exc_info)
     return [esxi_vsphere_server, esxi_user, esxi_pass]
+
+
+def list_vms(my_cluster):
+    #searcher = my_cluster.content.searchIndex
+    content = my_cluster.RetrieveContent()
+    for child in content.rootFolder.childEntity:
+        if hasattr(child, 'vmFolder'):
+            datacenter = child
+            vmFolder = datacenter.vmFolder
+            vmList = vmFolder.childEntity
+            for vm in vmList:
+                PrintVmInfo(vm)
+    print("finished")
+    return 0
+
+def PrintVmInfo(vm, depth=1):
+    """
+    Print information for a particular virtual machine or recurse into a folder
+    or vApp with depth protection
+    """
+    maxdepth = 10
+
+    # if this is a group it will have children. if it does, recurse into them
+    # and then return
+    if hasattr(vm, 'childEntity'):
+        if depth > maxdepth:
+            return
+        vmList = vm.childEntity
+        for c in vmList:
+            PrintVmInfo(c, depth+1)
+        return
+
+    # if this is a vApp, it likely contains child VMs
+    # (vApps can nest vApps, but it is hardly a common usecase, so ignore that)
+    if isinstance(vm, vim.VirtualApp):
+        vmList = vm.vm
+        for c in vmList:
+            PrintVmInfo(c, depth + 1)
+        return
+
+    summary = vm.summary
+    print("Name       : ", summary.config.name)
+    print("Path       : ", summary.config.vmPathName)
+    print("Guest      : ", summary.config.guestFullName)
+    annotation = summary.config.annotation
+    if annotation != None and annotation != "":
+        print("Annotation : ", annotation)
+    print("State      : ", summary.runtime.powerState)
+    if summary.guest != None:
+        ip = summary.guest.ipAddress
+        if ip != None and ip != "":
+            print("IP         : ", ip)
+    if summary.runtime.question != None:
+        print("Question  : ", summary.runtime.question.text)
+        print("")
+
+
 
 
 main()
